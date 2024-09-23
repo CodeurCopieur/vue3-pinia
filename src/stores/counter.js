f7bfba3a9cbbc8193e7695f7a3e2f308ebed5d44
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-09-24 00:27:40 +0200

    Définir le magasin à l'aide de la fonctionnalité de configuration de composition dans l'API de composition

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,23 +1,41 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
 
-export const useCounterStore = defineStore('counterStore', {
-  state: () => {
-    return {
-      count: 0
-    }
-  },
-  getters: {
-    doubleCount : (state) => state.count * 2
-  },
-  actions: {
-    increment() {
-      this.count++
-    }
+export const useCounterStore = defineStore('counterStore', () => {
+
+  const count = ref(0);
+  const doubleCount = computed(() => count.value * 2);
+
+  function increment() {
+    count.value++;
   }
+
+  return {
+    count,
+    doubleCount,
+    increment
+  }
+
 })
 
 
+
+//export const useCounterStore = defineStore('counterStore', {
+  // state: () => {
+  //   return {
+  //     count: 0
+  //   }
+  // },
+  // getters: {
+  //   doubleCount : (state) => state.count * 2
+  // },
+  // actions: {
+  //   increment() {
+  //     this.count++
+  //   }
+  // }
+  // })
 
 // export const useCounterStore = defineStore('counter', {
 //   state: ()=> ({
